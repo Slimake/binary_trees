1a55bde582b79cda315cb636b6aad4f3842bb9f0
Sunday Ajakaiye <marcusitovie@gmail.com>
2023-11-02 10:44:07 +0000

    Make betty complaint

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -2,20 +2,23 @@
 
 /**
  *  binary_tree_node - Creates a binary tree node
- *  
+ *
  *  @parent: pointer to the parent node of the node to create
  *  @value: value to insert in the new node
+ *
+ *	Return: the binary tree node
  */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-    binary_tree_t *newNode = malloc(sizeof(binary_tree_t));
-    if (!newNode)
-        return NULL;
-        
-    newNode->n = value;
-    newNode->parent = parent;
-    newNode->left = NULL;
-    newNode->right = NULL;
+	binary_tree_t *newNode = malloc(sizeof(binary_tree_t));
 
-    return newNode;
+	if (!newNode)
+		return (NULL);
+
+	newNode->n = value;
+	newNode->parent = parent;
+	newNode->left = NULL;
+	newNode->right = NULL;
+
+	return (newNode);
 }
