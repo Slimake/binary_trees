8b9512af05c5a5d3465a6dfcc919314168d7fc87
Sunday Ajakaiye <marcusitovie@gmail.com>
2024-06-04 15:37:19 +0100

    Betty style

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -2,7 +2,7 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_t *binary_tree_insert_left - Inserts a node at the 
+ * binary_tree_t *binary_tree_insert_left - Inserts a node at the
  * left-child of another node
  * @parent: Pointer to the parent node
  * @value: Value to put in the new node
@@ -13,20 +13,20 @@
 
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-    binary_tree_t *new_node_left;
+	binary_tree_t *new_node_left;
 
-    if (!parent)
-        return (NULL);
+	if (!parent)
+		return (NULL);
 
-    new_node_left = binary_tree_node(parent, value);
-    if (!new_node_left)
-        return (NULL);
+	new_node_left = binary_tree_node(parent, value);
+	if (!new_node_left)
+		return (NULL);
 
-    new_node_left->left = parent->left;
-    if (new_node_left->left)
-        new_node_left->left->parent = new_node_left;
+	new_node_left->left = parent->left;
+	if (new_node_left->left)
+		new_node_left->left->parent = new_node_left;
 
-    parent->left = new_node_left;
+	parent->left = new_node_left;
 
-    return (new_node_left);
+	return (new_node_left);
 }
