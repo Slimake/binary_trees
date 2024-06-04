46bfd2b14b205f441fd6df5b876a7cf0ca2d33a8
Sunday Ajakaiye <marcusitovie@gmail.com>
2024-06-04 17:09:47 +0100

    Check if parent is NULL

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -14,6 +14,9 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new_node_right;
 
+	if (!parent)
+		return (NULL);
+
 	new_node_right = binary_tree_node(parent, value);
 	if (!new_node_right)
 		return (NULL);
